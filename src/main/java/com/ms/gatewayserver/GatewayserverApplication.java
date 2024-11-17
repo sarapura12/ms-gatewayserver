2812ca15c74638cd9a21b8c125f3aba76eeb9e1d
jonatan <jonatansalva.js14@gmail.com>
2024-11-17 12:31:12 -0300

    se cambio la configuracion del gateway server utilizando bean

--- src/main/java/com/ms/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/ms/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.ms.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 
@@ -9,6 +14,34 @@ public class GatewayserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                .route(p -> p
+                        .path("/applications/apiclients/**")
+                        .filters(f ->
+                                f.rewritePath("/applications/apiclients/(?<segment>.*)","/${segment}")
+                                        .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://ms-clients")).
+
+
+                route(p -> p
+                        .path("/applications/apiproducts/**")
+                        .filters(f ->
+                                f.rewritePath("/applications/apiproducts/(?<segment>.*)","/${segment}")
+                                        .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://ms-products")).
+
+
+                route(p -> p
+                        .path("/applications/apisales/**")
+                        .filters(f ->
+                                f.rewritePath("/applications/apisales/(?<segment>.*)","/${segment}")
+            .addResponseHeader("X-Response-Time",new Date().toString()))
+            .uri("lb://ms-sales")).build();
     }
 
 }
